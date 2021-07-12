24cd617a038a1315632a6770a0d74e657c6d7c92
luizsilva <luizfsilva.091595@gmail.com>
2021-07-12 08:55:03 -0400

    enqueuePractice.js

---
+++ enqueuePractice.js
@@ -0,0 +1,11 @@
+// 1. Implement enqueue() in JS
+// Write a function that inserts a number to the tail with two arguments
+
+const ages = [7, 5, 1, 3, 4]
+
+function getEnqueue(array, num){
+    return array[array.length] = num
+}
+const num1 = 9
+getEnqueue(ages, num1)
+console.log(ages)
